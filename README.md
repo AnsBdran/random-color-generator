a1ea4887e366903481cda73b4f184c2703a69132
Anas Badran <ansaccts@gmail.com>
2022-12-06 01:17:40 +0200

    added favicon and readme
    added favicon, readme file and did a little bit of code cleaning

---
+++ README.md
@@ -0,0 +1,5 @@
+# Random Color Picker
+
+## click the `button` to get a random color, or type in the `input` the value you want and see the result immediately.
+
+### See the Live version [from here](https://ansbdran.github.io/random-color-generator/)
--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-const btn = document.querySelector('.color-input button')
-const colorInput = document.querySelector('.color-input input')
-const target = document.querySelector('.target')
-const input = document.querySelector('.color-input input')
-const root = document.querySelector(':root')
-const colorChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-
-
-console.log(btn, colorInput);
+const btn = document.querySelector('.color-input button');
+const colorInput = document.querySelector('.color-input input');
+const target = document.querySelector('.target');
+const input = document.querySelector('.color-input input');
+const root = document.querySelector(':root');
+const colorChars = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+];
 
 btn.addEventListener('click', () => {
-    let color = '#';
-    target.textContent = ''
-    for (let i = 0; i < 6 ; i++) {
-        const randomValue = Math.floor(Math.random() * 16)
-        color += `${colorChars[randomValue]}`
-        console.log(randomValue)
-    }
-    input.value = color
-        target.style.backgroundColor = color
-        input.style.color = color
-    root.style.setProperty('--color', color)
-})
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    const randomValue = Math.floor(Math.random() * 16);
+    color += `${colorChars[randomValue]}`;
+  }
+  paint(color);
+});
 
 input.addEventListener('keyup', (e) => {
-    target.textContent = ''
-    target.style.backgroundColor = e.target.value
-    input.style.color = e.target.value
-    root.style.setProperty('--color', e.target.value)
-})
+  paint(e.target.value);
+});
+
+const paint = (val) => {
+  target.textContent = '';
+  input.value = val;
+  root.style.setProperty('--color', val);
+  input.style.color = val;
+  target.style.backgroundColor = val;
+};
