49b7da7e972e5ceb1bd6dd8094e7542cc4f969dc
Anas Badran <ansaccts@gmail.com>
2022-12-06 09:23:32 +0200

    added the hover feature

--- index.js
+++ index.js
@@ -23,16 +23,21 @@ const colorChars = [
 ];
 
 btn.addEventListener('click', () => {
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    const randomValue = Math.floor(Math.random() * 16);
-    color += `${colorChars[randomValue]}`;
-  }
-  paint(color);
+  paint(getRandomColor());
 });
 
 input.addEventListener('keyup', (e) => {
   paint(e.target.value);
+});
+
+target.addEventListener('mouseover', () => {
+  const colorInterval = setInterval(() => {
+    paint(getRandomColor());
+    console.log('mouse in');
+  }, 500);
+  target.addEventListener('mouseout', () => {
+    clearInterval(colorInterval);
+  });
 });
 
 const paint = (val) => {
@@ -41,4 +46,13 @@ const paint = (val) => {
   root.style.setProperty('--color', val);
   input.style.color = val;
   target.style.backgroundColor = val;
+};
+
+const getRandomColor = () => {
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    const randomValue = Math.floor(Math.random() * 16);
+    color += `${colorChars[randomValue]}`;
+  }
+  return color;
 };
